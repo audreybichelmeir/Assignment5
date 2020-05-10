448de5ff5612ee4cda0a0b4384fd635b3e4ccc1d
audreybichelmeir <bichelmeir@chapman.edu>
2020-05-10 23:54:36 +0000

    working????

--- TreeNode.h
+++ TreeNode.h
@@ -1,112 +1,33 @@
-class TreeNode{
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+template <class T>
+class TreeNode {
 public:
   TreeNode();
   TreeNode(int k);
   //TreeNode(int k, <type> v);
   ~TreeNode();
-  //virtual ~TreeNode(); when working when a template class
+  //virtual ~TreeNode(); //- when working with a template class
 
   int key;
   TreeNode *left;
   TreeNode *right;
 };
 
-// treenode implementation
-TreeNode::TreeNode(){
+//treenode implementation
+template <class T>
+TreeNode<T>::TreeNode() {
   key = 0;
   left = NULL;
   right = NULL;
 }
 
-//i'll leave implementation of destructor to you
-
-class BST{
-  public:
-    BST();
-    ~BST();
-    //virtual ~BST() - when working with template classes
-
-    void insert(int value); //which in this example is also our key
-    void search(int value); //which in this example is also our key
-
-    bool isEmpty();
-    TreeNode* getMin();
-    TreeNode* getMax();
-
-    TreeNode* getSucccessor(TreeNode *d);
-    void printTree(TreeNode *node);
-
-  private:
-    TreeNode *root;
-};
-
-BST::BST()}{
-  root = NULL;
-}
-
-BST::~BST(){
-  //always lets build some charactter
-}
-
-void BST::printTree(TreeNode *node){ // big o run time - ?
-  if(node == NULL)
-    return;
-
-    printTree(node ->left);
-    cout << node-> key << endl;
-    printTree(node -> right);
-}
-
-TreeNode* BST::getMax(){
-  TreeNode *curr = root;
-
-  if(root == NULL){
-    return NULL;
-  }
-  while(curr ->right != NULL){
-    curr = curr->right;
-  }
-  return curr;
-}
-
-bool BST::isEmpty(){
-  return (root == NULL);
-}
-
-void BST::insert(int value){
-
-  if(search(value)){
-    cout << "value already exists" << endl;
-    return;
-  }
-
-  TreeNode *node = new TreeNode(value);
-
-  if(isEmpty){
-    //empty tree
-    root = node;
-  }else{
-    //not an empty tree
-    TreeNode* curr = root; // start at the root
-    TreeNode* parent;
-
-    while(true){
-    parent = curr;
-    if(value < curr-> key){
-      // go left
-      curr = curr->left;
-      if(curr == NULL){
-        parent->left = node;
-        break;
-        }
-      }else{ // either left or right
-        //go right & dont need to check =
-        curr = curr-> right;
-        if(curr = NULL){
-          parent->right = node;
-          break;
-        }
-      }
-    }
-  }
+template <class T>
+TreeNode<T>::TreeNode(int k){
+  key = k;
+  left = NULL;
+  right = NULL;
 }
